dfb05041546e0ceb14ee3f72f9c1951c69776edf
Ant0-n10 <antonio170pina@gmail.com>
2026-07-02 17:10:09 -0300

    feat(task): implement update and delete endpoints with existence validation

--- src/main/java/Ant0_n10/todolist/controllers/TaskController.java
+++ src/main/java/Ant0_n10/todolist/controllers/TaskController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +21,24 @@ public class TaskController {
     public ResponseEntity<Task> createTask(@Valid @RequestBody Task task){
        Task createdTask = taskService.createTask(task);
        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(){
+        List<Task> getAllTasks = taskService.getAllTasks();
+        return ResponseEntity.status(HttpStatus.OK).body(getAllTasks);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable long id, @Valid @RequestBody Task task){
+       Task updatedTask = taskService.updateTask(id, task);
+        return ResponseEntity.ok().body(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/Ant0_n10/todolist/services/TaskService.java
+++ src/main/java/Ant0_n10/todolist/services/TaskService.java
@@ -6,6 +6,9 @@ import Ant0_n10.todolist.repositorys.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class TaskService {
@@ -14,5 +17,25 @@ public class TaskService {
     public Task createTask(Task task) {
         task.setStatus(StatusTask.PENDING);
         return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+
+    public Task updateTask(Long id, Task task) {
+       Task taskUpdate = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Tarefa não encontrada com o ID: " + id));
+       taskUpdate.setDescription(task.getDescription());
+       taskUpdate.setStatus(task.getStatus());
+
+       return taskRepository.save(taskUpdate);
+    }
+
+    public void deleteTask(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new RuntimeException("Não é possível deletar. Tarefa não encontrada com o ID: " + id);
+        }
+        taskRepository.deleteById(id);
     }
 }
